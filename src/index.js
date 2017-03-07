ce9674ff8da3be3bd68f2ea6a382dac4adb3eeaa
Anithaa <anithag@mercerbell.com.au>
2017-03-07 12:39:27 +1100

    More functionality added

--- src/index.js
+++ src/index.js
@@ -11,7 +11,8 @@ module.exports = function (api_key, host) {
 
     // Default the host
     host = host || 'http://www.vision6.com.au/api/jsonrpcserver.php?version=3.0';
-
+	
+	
     if (!api_key) {
         throw new Error('Invalid Vision6 API Key');
     }
@@ -22,7 +23,7 @@ module.exports = function (api_key, host) {
     }
 
     var client = jayson.client[protocol](host);
-
+	
     return {
 
         /**
@@ -42,6 +43,8 @@ module.exports = function (api_key, host) {
                     debug(method_name, results);
 
                     if (typeof results.error !== 'undefined' && results.error) {
+                        console.log(options)
+						console.log(results)
                         throw new Error(results.error);
                     }
 
@@ -74,6 +77,183 @@ module.exports = function (api_key, host) {
                 offset,
                 sort_by,
                 sort_order
+            ]);
+        },
+		
+		//New methods added by Anitha
+		searchMessages: function (criteria, limit, offset, sort_by, sort_order) {
+            criteria   = criteria || [];
+            limit      = limit || 100;
+            offset     = offset || 0;
+            sort_by    = sort_by || 'name';
+            sort_order = sort_order || 'ASC';
+
+            return this.call('searchMessages', [
+                criteria,
+                limit,
+                offset,
+                sort_by,
+                sort_order
+            ]);
+        },
+		
+		//Get Message By ID
+		getMessageByID: function (messageID) {
+             messageID   = messageID || 892769;
+
+            return this.call('getMessageById', [
+                messageID               
+            ]);
+        },
+		
+		//Get Batch By Queue ID
+		getBatchByQueueID: function (queueID) {
+			
+             queueID    = queueID  || 2162782;
+			
+            return this.call('getBatchIdByQueueId', [
+                queueID                
+            ]);
+        },
+		
+		//Get Batch Status
+		getBatchStatus: function (batchID) {
+			
+             batchID    = 2162783;
+	
+            return this.call('getBatchStatus ', [
+                batchID                
+            ]);
+        },
+
+
+        //Add Folder
+		addFolder: function (name) {
+
+			name   = name || "temp";
+            
+            var folder_details       =
+                {
+                    "name": "as",
+                    "type": 'message'
+                };
+          
+            
+            return this.call('addFolder ', [
+                folder_details                
+            ]);
+        },
+
+        //Add List
+		addList: function (folder_id, name, on_register_address ) {
+			folder_id   = folder_id || 366587;
+            on_register_address      = on_register_address || 'anithag@mercerbell.com.au';
+            name     = name || "Temp List";
+          
+            var list_details      = {
+                "folder_id": folder_id,
+                "name": name,             
+                "on_register_address": on_register_address
+            };
+
+       
+            return this.call('addList ', [
+                list_details                
+            ]);
+        },
+
+         //Search Folder
+		searchFolders: function (folder_type) {
+			
+          
+           folder_type   = folder_type || 'list';
+
+           
+            return this.call('searchFolders ', [
+                folder_type                
+            ]);
+        },
+
+         //Delete List
+		deleteList: function (listID) {
+           
+            return this.call('deleteList ', [
+                listID                
+            ]);
+        },
+
+          //Search Folder
+		searchBatches: function (batchID) {
+            
+            var searchC = [[
+                "id",
+                "exactly",
+                batchID
+            ]];
+
+          
+            return this.call('searchBatches ', [
+                    searchC 
+            ]);
+        },
+		
+		////Add Batch
+		addBatch: function (messageID, listid,  send_time, is_test) {
+            messageID   = messageID || 892769;
+            var  batch_details      = [{
+                "list_id": listid,
+                "type": "list",             
+                "time": "now"
+            }];
+           
+		
+            return this.call('addBatch', [
+                892769,
+                batch_details,
+                1488324760,
+                true
+            ]);
+        },
+
+        ////Add Contacts
+		addContacts: function (listID, contacts) {
+            listID   = listID || 892769;
+            contacts  = contacts;
+           
+            return this.call('addContacts', [
+                listID,
+                contacts
+            ]);
+        },
+		
+
+         ////Add Contacts to Subscriptionlist
+		addContactsToSubscriptionList: function (listID, contacts) {
+            listID   = listID || 892769;
+            contacts  = contacts;
+           
+            return this.call('addContacts', [
+                listID,
+                contacts
+            ]);
+        },
+
+
+         ////Add Contacts to Subscriptionlist
+		countContacts: function (listID) {
+            listID   = listID 
+           
+            return this.call('countContacts', [
+                listID
+            ]);
+        },
+
+         ////Clear List
+		clearList: function (listID) {
+            console.log(listID)
+            listID   = listID 
+            return this.call('clearList', [
+                listID
             ]);
         }
     };
